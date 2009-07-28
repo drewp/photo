b3179fd853a1d61b9645f7125db7d15ba2b641f2
drewp <drewp@bigasterisk.com>
2009-07-28 01:56:27 -0700

    try a gamin version of fileschanged, but it doesn't recurse. Don't stop on callback errors.
    Ignore-this: 4dd32e68c4869ebcc6954504a57182f1
    
    darcs-hash:20090728085627-312f9-6efa101416e7246083ac7139c843bd894ae98239.gz

--- fileschanged.py
+++ fileschanged.py
@@ -1,7 +1,7 @@
 """
 wrapper for the fileschanged command
 """
-import os, subprocess, logging
+import time, os, subprocess, logging, traceback
 log = logging.getLogger('fileschanged')
 log.setLevel(logging.DEBUG)
 
@@ -17,16 +17,44 @@ def fileschanged(topDirs, callback):
     for line in iter(proc.stdout.readline, None):
         filename = line.strip()
         log.debug("fileschanged: %s" % filename)
-        callback(filename)
+        try:
+            callback(filename)
+        except Exception, e:
+            log.error("Error during callback for %s" % filename)
+            traceback.print_exc()
+
+def fileschanged_gamin(topDirs, callback):
+    """version using gamin, which does not have recursive support. I'd
+    have to find subdirs, then add dynamic watches to any new dirs
+    that show up."""
+    import gamin
+    mon = gamin.WatchMonitor()
+    def cb(path, event, data):
+        fullpath = os.path.join(data, path)
+        log.debug("fileschanged: %s" % fullpath)
+        return callback(fullpath)
+    for d in topDirs:
+        mon.watch_directory(d, cb, d)
+    while 1:
+        mon.handle_events()
+        time.sleep(1)
+    print "done"
+
 
 def allFiles(topDirs, callback):
     """call callback on every file under all the given dirs. Skips dot files."""
     for topDir in topDirs:
         for root, dirs, files in os.walk(topDir):
+            log.debug("allFiles: %s" % root)
             for filename in files:
                 # the dot hides emacs partial-save files, which i
-                # didn't notice anyway
+                # didn't notice anyway. todo: this isn't finding dot-directories
                 if filename.startswith('.'):
                     continue
                 filename = os.path.join(root, filename)
-                callback(filename)
+                try:
+                    callback(filename)
+                except Exception, e:
+                    log.error("Error during callback for %s" % filename)
+                    traceback.print_exc()
+                    
