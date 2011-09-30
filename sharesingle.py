2a26bf3ae24fb0dec20290aa4a57ec74e36281dd
drewp <drewp@bigasterisk.com>
2011-09-29 19:00:56 -0700

    sharesingle standalone server
    Ignore-this: 76fc5f3e2b86a2d045e4be59e22174d6
    
    darcs-hash:20110930020056-312f9-c74e73b38f383e2af8db12473f74fb1565efa102.gz

--- sharesingle.py
+++ sharesingle.py
@@ -1,34 +1,50 @@
+import web
 from genshi import Markup
 from genshi.template import TemplateLoader
 from genshi.output import XHTMLSerializer
 import networking, photos
 from urls import localSite
+from oneimagequery import photoCreated
+import db
+from ns import SITE
 
 loader = TemplateLoader(".", auto_reload=True)
 serializer = XHTMLSerializer()
+
+class Index(object):
+    def GET(self):
+        return "sharesingle"
 
 class ShareSingle(object):
     """
     image page that features one main image (but maybe has links to
     related ones) and is designed for casual or new users who don't
-    know anything about the photo site. The viewer may not be logged in.
+    know anything about the photo site. The viewer may not be logged
+    in. It has already been determined that this user may see this
+    image.
     """
-    def __init__(self, graph, uri):
-        self.graph, self.uri = graph, uri
+    def GET(self, relImageUri):
+        uri = SITE[relImageUri]
+        print "abs", uri
+        size = photos.getSize(uri, photos.sizes["screen"])
 
-    def render(self, cookie, forwardedFor):
-
-        size = photos.getSize(self.uri, photos.sizes["screen"])
-        
+        try:
+            created = photoCreated(graph, uri)
+            prettyDate = created.date().isoformat()
+        except ValueError:
+            prettyDate = "(unknown date)"
+            
         tmpl = loader.load("sharesingle.html")
         stream = tmpl.generate(
             title="photo",
-            greet="Kelsi has shared a picture with you",
-            bestJqueryLink=networking.jqueryLink(forwardedFor),
+            prettyDate=prettyDate,
+            bestJqueryLink=networking.jqueryLink(
+                web.ctx.environ.get('HTTP_X_FORWARDED_FOR', '')),
             featuredImg=Markup('<img src="%s" width="%s" height="%s"/>' %
-                               (localSite(self.uri)+"?size=screen",
+                               (localSite(uri)+"?size=screen",
                                 size[0], size[1])),
-            loginWidget=Markup(networking.getLoginBarSync(cookie)),
+            loginWidget=Markup(networking.getLoginBarSync(
+                web.ctx.environ.get('HTTP_COOKIE', ''))),
             actionsAllowed=[],
             otherSizeLinks=[],
             link="l",
@@ -36,4 +52,12 @@ class ShareSingle(object):
             pageJson="",
             )
         return (''.join(serializer(stream))).encode('utf8')
+
+
+graph = db.getGraph()
+app = web.application((
+    r'/', 'Index',
+    r'/(.*)/single', 'ShareSingle',
+    ), globals())
+application = app.wsgifunc()
 
