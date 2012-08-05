0cbc7d39796bd1dde33de4fff1839381f4c313e1
drewp <drewp@bigasterisk.com>
2012-08-05 14:50:21 -0700

    celery config attempt
    Ignore-this: b58c35d190497c6b56f7ce8fa4970b1e
    
    darcs-hash:20120805215021-312f9-07db51e052b6273c97cc185fe34ef822b1c56bcf.gz

--- encode/worker.py
+++ encode/worker.py
@@ -8,9 +8,19 @@ sys.path.append("..")
 import photos
 
 celery = Celery('worker')
+celery.conf.update(dict(
+        CELERY_RESULT_BACKEND = "mongodb",
+        CELERY_MONGODB_BACKEND_SETTINGS = {
+            "host": "bang",
+            "database": "photoQueue",
+            }
+        ))
 
 @celery.task
 def justCache(uri, sizes):
     #current_task.update_state(state='PROGRESS', meta={'current': 'x'})
     photos.justCache(uri, sizes)
 
+
+if __name__ == '__main__':
+    celery.worker_main()
