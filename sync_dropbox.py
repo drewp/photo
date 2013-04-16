d95f5f5889a1a992e4bccabbb5b98a0fd18ad3bb
drewp <drewp@bigasterisk.com>
2013-04-15 23:47:29 -0700

    dropbox ingester: better logging; some fix to how new files are made
    Ignore-this: b3bd5c59da5722cd171dbefe18767660
    
    darcs-hash:20130416064729-312f9-fdbe88b21d649a4ad665f07bbae086af5c989e96

--- sync_dropbox.py
+++ sync_dropbox.py
@@ -9,42 +9,44 @@ sync to shared disk with names like
 '2013-01-13T16-04-56.jpg'
 '2013-01-11T15-10-03-1.jpg'
 
-
-bin/python sync_dropbox.py /home/drewp/Dropbox/Camera\ Uploads\ from\ Kelsi /my/pic/phonecam/ki8
-
-
-
 """
+import boot
 import sys, os, argparse
-from twisted.python.filepath import FilePath
+from twisted.python.filepath import FilePath, _secureEnoughString
+from fileschanged import fileschanged
+log = boot.log
 
 def copy(source, dest, mock):
     if mock:
-        print "would copy %s to %s" % (f.path, out.path)
+        log.info("would copy %s to %s" % (source.path, dest.path))
         return
         
-    if out.exists():
-        if out.getsize() == source.getsize():
-            print "%s exists. no action." % out.path
+    if dest.exists():
+        if dest.getsize() == source.getsize():
+            log.info("%s exists. no action." % dest.path)
             return
 
     try:
-        outDir.makedirs()
+        os.makedirs(dest.dirname())
     except OSError:
         pass
 
-    out.setContent(f.open().read())
-    out.chmod(0644)
-    os.utime(out.path, (f.getModificationTime(),
-                        f.getModificationTime()))
-    print "wrote %s" % out.path
+    # dotfiles are ignored by the other fileschanged process that is
+    # watching for files to ingest
+    tempDest = dest.sibling("."+dest.basename()+_secureEnoughString())
+    source.copyTo(tempDest)
+    tempDest.chmod(0644)
+    os.utime(tempDest.path, (source.getModificationTime(),
+                             source.getModificationTime()))
+    tempDest.moveTo(dest)
+    log.info("wrote %s" % dest.path)
 
 def sync(f, outDir, newerthan, mock):
     out = outDir.child(f.basename().replace(' ', 'T').replace('.', '-', 2))
 
     if newerthan and out.basename() <= newerthan:
-        print "skipping file %s; older than cutoff" % out.path
-        continue
+        log.info("skipping file %s; older than cutoff" % out.path)
+        return
 
     copy(f, out, mock)
     
@@ -54,15 +56,19 @@ parser.add_argument('--newerthan',
     help="ignore files whose output names don't sort after this string")
 parser.add_argument('--destdir', help='output dir')
 parser.add_argument('--mock', action='store_true', help='mock mode')
-parser.add_argument('--input', help='input filename')
+parser.add_argument('--input', help='input filename or dir-to-watch')
 args = parser.parse_args()
 
 outDir = FilePath(args.destdir)
 
-if os.path.isdir(args.input):
-    files = FilePath(args.input).children()
+if not os.path.isdir(args.input):
+    # single-file mode
+    sync(FilePath(args.input), outDir, args.newerthan, args.mock)
 else:
-    files = [FilePath(args.input)]
+    for f in sorted(FilePath(args.input).children()):
+        sync(f, outDir, args.newerthan, args.mock)
 
-for f in files:
-    sync(f, outDir, args.newerthan, args.mock)
+    def onChange(filename):
+        sync(FilePath(filename), outDir, args.newerthan, args.mock)
+    fileschanged([args.input], onChange)
+
