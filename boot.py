98adbc0cf08944844ead8f5f799f98b858e530f9
drewp <drewp@bigasterisk.com>
2014-07-13 01:47:40 -0700

    no more local monque with setproctitle patch
    Ignore-this: 4bffcfc918542b880cfffcef920e40c2
    
    darcs-hash:20140713084740-312f9-19bfce562d2ac73b411e34170584a0394dfb2817

--- boot.py
+++ boot.py
@@ -11,7 +11,6 @@ log = getLogger()
 
 sys.path.insert(0, "/home/drewp/projects/sparqlhttp")
 sys.path.insert(0, "/my/proj/sparqlhttp")
-sys.path.insert(0, "/my/dl/modified/monque")
 
 getLogger("rdflib").setLevel(WARN) # for a version announcement that comes out on info /my/site/photo/local/lib/python2.7/site-packages/rdflib/__init__.py
 import sparqlhttp
