ebe45cafffafa4d5afc2eb7e30aab29b6f8dc215
drewp <drewp@bigasterisk.com>
2016-02-08 03:32:59 -0800

    photo-thumbnail no-link mode
    Ignore-this: d1d6ed5a350d38b17fe26dce6ff691a1
    
    darcs-hash:584cdb9ef30612759889d7294f13442e62212581

--- v2/elements/photo-thumbnail.js
+++ v2/elements/photo-thumbnail.js
@@ -1,24 +1,38 @@
 Polymer({
     is: 'photo-thumbnail',
     properties: {
-        img: {
+        img: { // RDF uri
             notify: true,
             observer: 'imgChanged'
         },
-        size: { notify: true }
+        uri: { // link to page
+            notify: true,
+        },
+        size: { notify: true },
+        imgSrc: { // image thumb
+            notify: true,
+        },
+        noLink: {
+            type: Boolean,
+            notify: true
+        }
     },
     imgChanged: function (img) {
         if (!img) {
             this.imgSrc = this.uri = null;
             return;
         }
-        this.uri = typeof img == 'string' ? img : img.uri;
+        var uri = typeof img == 'string' ? img : img.uri;
         this.imgSrc = '';
         var size = this.size || 'small';
-        this.async(function () {
-            this.imgSrc = this.uri.replace('http://', 'https://') + '?size=' + size;
-        }, null, 1);  // video marker, uri link, click action
+        this.imgSrc = uri.replace('http://', 'https://') + '?size=' + size;
+        // video marker, uri link, click action
         // preload control, etc
+        if (this.noLink) {
+            this.uri = null;
+        } else {
+            this.uri = uri;
+        }
     },
     computeHref: function (uri) {
         return uri + '/page';
